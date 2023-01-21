76af3523344dd563de8168be927637c1ac7ea26e
Harinder <harindersingh2107@gmail.com>
2023-01-21 16:30:59 +0530

    msg rout added and email send removed

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, redirect, request, flash,send_from_directory
+from flask import Flask, render_template, redirect, request, flash, send_from_directory
 from flask_bootstrap import Bootstrap
-from forms import UserForm,LoginForm
+from forms import UserForm, LoginForm
 from EditForms import *
-from database import db, create_all, Bio, Education, LicenseCertification, Project, Experience, Award, Skill, UserMsg,sampledata
+from database import db, create_all, Bio, Education, LicenseCertification, Project, Experience, Award, Skill, UserMsg, \
+    sampledata
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from notifier import send_mail,msg_format
+from notifier import send_mail, msg_format
 from dotenv import load_dotenv
+from datetime import date
 import os
+
 load_dotenv()
 
-users = {'admin': {'password': 'pbkdf2:sha256:260000$DQS0DPTWGn0eHkvK$82e919a0a59d1c5f6f1fa4d5acbb7f2b25d7161635ad07dcfc09fb59376af692',
-                   'active': True}
+users = {'admin': {
+    'password': 'pbkdf2:sha256:260000$DQS0DPTWGn0eHkvK$82e919a0a59d1c5f6f1fa4d5acbb7f2b25d7161635ad07dcfc09fb59376af692',
+    'active': True}
          }
 
 app = Flask(__name__)
@@ -28,8 +32,10 @@ with app.app_context():
     # sampledata()
     pass
 
+
 class User(UserMixin):
     pass
+
 
 # Load the user
 @login_manager.user_loader
@@ -46,7 +52,7 @@ def load_user(username):
 @app.route("/")
 def home():
     projects = Project.query.order_by(Project.ranking.asc()).all()[:3]
-    skills =  Skill.query.order_by(Skill.ranking.asc()).all()
+    skills = Skill.query.order_by(Skill.ranking.asc()).all()
     bio = db.session.query(Bio).all()
     form = UserForm()
     return render_template('index.html', projects=projects, skills=skills, bio=bio, form=form)
@@ -57,23 +63,31 @@ def projects():
     all_projects = Project.query.order_by(Project.ranking.asc()).all()
     return render_template('projects.html', projects=all_projects)
 
+
 @app.route("/cv")
 def cv():
     return render_template("cv.html")
+
+
 @app.route('/download')
 def download():
     return send_from_directory('static', path="files/HarinderSingh.pdf")
+
+
 @app.route("/dataHandle", methods=["GET", "POST"])
 def user_data():
     form = request.form
-    new_msg = UserMsg(email=form['email'], massage=form['massage'])
+    new_msg = UserMsg(email=form['email'], massage=f"On {str(date.today())} read as {form['massage']}")
     db.session.add(new_msg)
     db.session.commit()
-    msg = msg_format(from_p="My Portfolio",
-                     to="me",subject="New massage from Portfolio.", body=f"{form['email']} \n{form['massage']}")
-    send_mail(to_ad= os.environ.get("TO_EMAIL"),massage=msg)
+    # msg = msg_format(from_p="My Portfolio",
+    #                  to="me", subject="New massage from Portfolio.", body=f"{form['email']} \n{form['massage']}")
+    # Email sending not working with render
+    # send_mail(to_ad= os.environ.get("TO_EMAIL"),massage=msg)
     flash(message="Received your Massage. Thank you!", category="information")
     return redirect(app.url_for('home'))
+
+
 # --------------------------------------login route setup---------
 
 
@@ -85,29 +99,37 @@ def login():
         password = request.form.get('password')
 
         if username not in users:
-            flash(message="No user Exist",category="error")
-            return render_template("login.html",form=form)
+            flash(message="No user Exist", category="error")
+            return render_template("login.html", form=form)
 
         user = User()
         user.id = username
 
-
         if check_password_hash(users[username]['password'], password):
             login_user(user)
-            flash(message="Login success",category="information")
+            flash(message="Login success", category="information")
             return redirect(app.url_for('home'))
         else:
             flash(message="Password does not match,Try again!", category="error")
-            return render_template("login.html",form=form)
+            return render_template("login.html", form=form)
 
-    return render_template("login.html",form=form)
+    return render_template("login.html", form=form)
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(app.url_for('home'))
 
+
 # ---------------------------------login-required--------------------------------
+@app.route("/msg")
+@login_required
+def all_msg():
+    msgs = db.session.query(UserMsg).all()
+    return render_template("msg.html", messages=msgs)
+
+
 @app.route('/edit/<string:model_name>/<int:model_id>', methods=['GET', 'POST'])
 @login_required
 def edit(model_name, model_id):
@@ -204,6 +226,11 @@ def delete(model_name, model_id):
         db.session.commit()
         flash('Skill has been deleted', 'success')
         return redirect(app.url_for('home'))
+    elif model_name == 'usermsg':
+        model = UserMsg.query.get(model_id)
+        db.session.delete(model)
+        db.session.commit()
+        return redirect(app.url_for('all_msg'))
 
 
 if __name__ == '__main__':
